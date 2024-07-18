cfd1d76cb4f6cbca6423f1b5b98a6a120c5b8bb9
Anson K Ekau <ansonekau@gmail.com>
2024-07-17 22:32:28 -1000

    made plot interactive with plotly

--- shinyapp/shinyscript.R
+++ shinyapp/shinyscript.R
@@ -5,6 +5,7 @@ library(here)
 library(shiny)
 library(bslib)
 library(bsicons)
+library(plotly)
 
 
 
@@ -34,14 +35,17 @@ ui <- page_sidebar(
   
   layout_columns(
     
-  card(card_header("Output"),
-       plotOutput("linePlot",
-       dblclick = "linePlot_dblclick",
-       brush = brushOpts(
-         id = "linePlot_brush",
-         resetOnNew = TRUE
-       )
-      )
+    card(card_header("Output"),
+         plotlyOutput("linePlot")
+                      
+  # card(card_header("Output"),
+  #      plotOutput("linePlot",
+  #      dblclick = "linePlot_dblclick",
+  #      brush = brushOpts(
+  #        id = "linePlot_brush",
+  #        resetOnNew = TRUE
+       
+      
   ),
   
   col_widths = c(12) 
@@ -51,37 +55,39 @@ ui <- page_sidebar(
 server <- function(input, output, session) {
   
   
-  ranges <- reactiveValues(x = NULL, y = NULL)
+  #ranges <- reactiveValues(x = NULL, y = NULL)
   # Update site_specific choices based on selected site
   observe({
     selected_site <- input$site
     updateSelectInput(session, "site_specific", choices = unique(data[data$site == selected_site, "site_specific"]))
   })
   
-  observeEvent(input$linePlot_dblclick, {
-    brush <- input$linePlot_brush
-    if (!is.null(brush)) {
-      ranges$x <- c(brush$xmin, brush$xmax)
-      ranges$y <- c(brush$ymin, brush$ymax)
-      
-    } else {
-      ranges$x <- NULL
-      ranges$y <- NULL
-    }
-  })
+  # observeEvent(input$linePlot_dblclick, {
+  #   brush <- input$linePlot_brush
+  #   if (!is.null(brush)) {
+  #     ranges$x <- c(brush$xmin, brush$xmax)
+  #     ranges$y <- c(brush$ymin, brush$ymax)
+  #     
+  #   } else {
+  #     ranges$x <- NULL
+  #     ranges$y <- NULL
+  #   }
+  # })
   
-  output$linePlot <- renderPlot({
+  output$linePlot <- renderPlotly({
     filtered_data <- data %>%
       filter(site == input$site,
              site_specific == input$site_specific,
              variable == input$variable)
     
-    ggplot(filtered_data, aes(x = date_time_hst, y = value)) +
+   p <- ggplot(filtered_data, aes(x = date_time_hst, y = value)) +
       geom_line() +
       labs(title = paste("Line Graph of", input$variable, "at", input$site, "-", input$site_specific),
            x = "Date Time",
-           y = input$variable) +
-      coord_cartesian(xlim = ranges$x, ylim = ranges$y, expand = FALSE)
+           y = input$variable) #+
+      #coord_cartesian(xlim = ranges$x, ylim = ranges$y, expand = FALSE)
+   
+    ggplotly(p)
   })
 }
 
